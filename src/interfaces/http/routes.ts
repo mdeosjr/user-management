fa7127817860197427092ebd95607f023a67ea74
Moises Jr <m.junior@eq.ufrj.br>
2023-06-23 00:41:20 -0300

    feat: update user route

--- src/interfaces/http/routes.ts
+++ src/interfaces/http/routes.ts
@@ -5,6 +5,7 @@ import * as authController from "@controllers/authController";
 const router = Router();
 
 router.post('/users', userController.createUser);
+router.put('/users', userController.updateUser);
 router.post('/login', authController.login)
 
 export default router;
