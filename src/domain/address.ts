2b9b373d1091b025c6a5838956e59f071d23b341
Moises Jr <m.junior@eq.ufrj.br>
2023-06-24 23:02:32 -0300

    feat: add cep validation

--- src/domain/address.ts
+++ src/domain/address.ts
@@ -1,7 +1,8 @@
+import getCepInfo from "@infra/api/getCepInfo";
 export class Address {
 	street: string;
 	number: string;
-	neighbourhood: string;
+	neighborhood: string;
 	city: string;
 	uf: string;
 	cep: string;
@@ -9,16 +10,27 @@ export class Address {
 	constructor(
 		street: string,
 		number: string,
-		neighbourhood: string,
+		neighborhood: string,
 		city: string,
 		uf: string,
 		cep: string
 	) {
+		this.validateCep(cep);
 		this.street = street;
 		this.number = number;
-		this.neighbourhood = neighbourhood;
+		this.neighborhood = neighborhood;
 		this.city = city;
 		this.uf = uf;
 		this.cep = cep;
+	}
+
+	async validateCep(cep: string): Promise<void> {
+		if (!cep) throw { type: "bad_request", message: "CEP missing!" };
+
+		const address = await getCepInfo(cep);
+		if (address.erro) throw { type: "conflict", message: "Address not found!" };
+
+		if (address.uf !== "AM")
+			throw { type: "conflict", message: "You are outside the AM state!" };
 	}
 }
