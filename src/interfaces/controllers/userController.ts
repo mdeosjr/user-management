7ff43f6a2f4a7da865793b58c6881ef18c0618ea
Moises Jr <m.junior@eq.ufrj.br>
2023-06-26 00:33:11 -0300

    refactor: changes response data

--- src/interfaces/controllers/userController.ts
+++ src/interfaces/controllers/userController.ts
@@ -13,7 +13,13 @@ export async function getUser(req: Request, res: Response) {
 	const token = authorization?.replace("Bearer ", "") as string;
 
 	const user = await userService.getUser(token);
-	res.status(200).send(user);
+	const { address, ...userData } = user;
+	res.status(200).send({
+		name: userData.name,
+		email: userData.email,
+		age: userData.age,
+		address,
+	});
 }
 
 export async function updateUser(req: Request, res: Response) {
@@ -21,8 +27,8 @@ export async function updateUser(req: Request, res: Response) {
 	const authorization = req.headers.authorization;
 	const token = authorization?.replace("Bearer ", "") as string;
 
-  await userService.updateUser(updatedUser, token);
-  res.sendStatus(200);
+	await userService.updateUser(updatedUser, token);
+	res.sendStatus(200);
 }
 
 export async function updateAddress(req: Request, res: Response) {
